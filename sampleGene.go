d5cfb4fb6ce2c344f7ce57226f4c45fffcf330e2
copywrite <woyaozhucele@gmail.com>
2013-12-03 13:09:21 +0800

    Create sampleGene.go

---
+++ sampleGene.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	o, err := os.Create("sampleData.txt")
+	if err != nil {
+		fmt.Printf("open error: %v\n", err)
+	}
+
+	for i := 0; i < 40000; i++ {
+		// fmt.Println(randomString(10))
+		o.WriteString(strings.ToLower(randomString(8)) + "\n")
+	}
+
+	o.Close()
+
+}
+
+func randomString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
